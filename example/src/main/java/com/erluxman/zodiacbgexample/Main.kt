7be60d8e0dae1d0be255e1e58943690bbae0c493
Laxman Bhattarai <erlaxmanbhattarai@gmail.com>
2017-06-06 12:06:03 +0545

    successfully hosted the rashifal bg library

--- example/src/main/java/com/erluxman/zodiacbgexample/Main.kt
+++ example/src/main/java/com/erluxman/zodiacbgexample/Main.kt
@@ -2,13 +2,12 @@ package com.erluxman.zodiacbgexample
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.example.zodiacbg.RashiBGView
 
 private class Main : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val v: RashiBGView = findViewById(R.id.background) as RashiBGView
+
     }
 }
